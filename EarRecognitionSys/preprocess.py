1ead80da3099e2c045e430ede88f76a9c25b2a98
DrewS <1251956930@qq.com>
2019-04-17 14:59:12 +0800

    dbqdbq

--- EarRecognitionSys/preprocess.py
+++ EarRecognitionSys/preprocess.py
@@ -2,24 +2,22 @@ import numpy as np
 import cv2
 
 
-def filling(dir_path,datasize):
-    for i in datasize:
-        img = cv2.imread(dir_path + i +'.jpg',1)#flag > 0 to load color
-
-        if(img.cols > img.rows):
-            filling_size = img.cols
-            res_size = img.rows
+def filling(dir_path_src,dir_path_tar,datasize):
+   for i in datasize:
+        img = cv2.imread(dir_path_src + i +'.jpg',1)#flag > 0 to load color
+        if(img.shape[0] > img.shape[1]):
+            filling_size = img.shape[0]
+            res_size = img.shape[1]
             background = np.zeros((filling_size, filling_size, 3), np.uint8)
-            background[:, (filling_size - res_size) / 2: filling_size + (filling_size - res_size) / 2] = img
+            background[:, (filling_size - res_size) / 2: (filling_size - res_size) / 2 + res_size, :] = img
         else:
-            filling_size = img.rows
-            res_size = img.cols
+            filling_size = img.shape[1]
+            res_size = img.shape[0]
             background = np.zeros((filling_size, filling_size, 3), np.uint8)
-            background[(filling_size - res_size) / 2: filling_size + (filling_size - res_size) / 2, :] = img
+            background[(filling_size - res_size) / 2: res_size + (filling_size - res_size) / 2, :] = img
 
-        cv2.resize(background, (224, 224))
-        cv2.imwrite(dir_path + i +'.jpg',img)
-
+        cv2.resize(background, (224, 224), cv2.INTER_CUBIC)
+        cv2.imwrite(dir_path_tar + i + '.jpg',background)
 def mean(dir_path,datasize):
     sum = np.zeros((224, 224, 3), np.uint8)
     for i in datasize:
@@ -32,7 +30,6 @@ def mean(dir_path,datasize):
         img = cv2.imread(dir_path + i + '.jpg', 1)
         img = cv2.subtract(img,m)
         cv2.imwrite(dir_path + i +'.jpg',img)
-
 
 
 
